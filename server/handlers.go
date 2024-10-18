1d7c21c663e4499925bd029bdc618892f4dd9a34
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-18 00:20:36 -0500

    update README

--- server/handlers.go
+++ server/handlers.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -48,5 +49,44 @@ func streamOutput(w http.ResponseWriter, content string) {
 		}
 		w.Write([]byte(response.Choices[0].Delta.Content))
 		w.(http.Flusher).Flush()
+	}
+}
+
+func returnStream(w http.ResponseWriter, content string) {
+	stream, err := logic.ChatStream(os.Getenv("OPENAI_API_KEY"), content); if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	defer stream.Close()
+
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.(http.Flusher).Flush()
+
+	// Flush the headers to ensure they are sent immediately
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+
+	for {
+		response, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return // End of stream
+		}
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Format the output for SSE: data: <content>\n\n
+		msg := fmt.Sprintf("data: %s\n\n", response.Choices[0].Delta.Content)
+		w.Write([]byte(msg))
+
+		// Flush to ensure the client gets the message immediately
+		if flusher, ok := w.(http.Flusher); ok {
+			flusher.Flush()
+		}
 	}
 }
